4e4e8b7dff80ac26dfc4909db74ec1bf8275c499
Matías Hillmann <mailmatiash@gmail.com>
2023-08-23 19:19:24 -0300

    agrego cosas

---
+++ p4/contador_palabras.c
@@ -0,0 +1,34 @@
+#include <stdlib.h> /*la nueva libreria*/
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char c,primera, ultima;  //o int c;
+    int cont=0;
+    printf("Ingrese una frase que termine con '.'\n");
+    c = getchar();
+    // buenas tardes  .  palabras=2 espacios=4
+    while(c!='.'){ //no es mejor con los 2 while?
+
+        while(c==' '){
+            c=getchar();
+        }
+
+        while(c!=' ' && c!='.'){//hola
+            c=getchar();
+            if(c==' ' || c=='.'){
+                cont++;
+            }
+
+        }
+        putchar(c);
+
+    }
+    printf("\nEl cont= %d \n %c", cont,c);
+
+
+
+    return 0;
+}
+
---
+++ p4/ej5_clase.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int main(){
+     int c,cont=0,aux,primera;
+     printf("ingrese la frase \n");
+     c=getchar();
+     while(c!='.'){
+         primera=c;
+         while(c!=' ' && c!='.'){
+             aux=c;
+             c=getchar();
+         }
+         if (aux==primera)
+             cont++;
+         aux=' '; 
+         primera=' ';
+         if (c!='.')
+             c=getchar();
+         while(c==' ')
+             c=getchar();
+         
+     }
+     printf("\nla cantidad de palabras que empiezan y terminan con la misma letra es %d",cont);
+ }
