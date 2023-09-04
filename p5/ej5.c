e9cf9bb91857e3b591089c3cd38195fb4e4920dd
Matías Hillmann <mailmatiash@gmail.com>
2023-09-04 15:49:08 -0300

    agrego ej1 p5 comments

---
+++ p5/ej5.c
@@ -0,0 +1,46 @@
+/*
+
+******************
+
+pasaje ref
+
+*int val
+
+hago cosas con &
+
+( val) (pq ya es puntero)
+
+******************
+
+if (i)
+	if (j)
+		if (k)
+
+
+******************
+
+main
+crearMatrices(A,B,C) <- inicializarMatrices() (C con el neutro para lo q queremos hacer)
+cargatMatrices(A,B)
+multiplicarMatrices()
+printf("el Resultado de A*B es:\n")
+mostrarMatriz(C)
+
+******************
+
+A e m*n, B e n*p, C e m*p
+
+
+******************
+
+*/
+
+#include <stdio.h>
+
+
+int main(void) {
+
+
+
+	return 0;
+}
