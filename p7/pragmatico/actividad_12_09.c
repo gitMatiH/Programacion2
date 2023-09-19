3037c6880439b9c882e887665020aae616e98ee4
Matías Hillmann <mailmatiash@gmail.com>
2023-09-18 22:27:17 -0300

    agrego

--- p7/pragmatico/actividad_12_09.c
+++ p7/pragmatico/actividad_12_09.c
@@ -38,6 +38,11 @@ typedef struct nodoListaProductos {
 	struct nodoListaProductos* siguiente;
 } nodoListaProductos;
 
+typedef struct nodoListaTicket {
+	infoTicket datos;
+	struct nodoListaTicket* siguiente;
+} nodoListaTicket;
+
 /*
 typedef struct listaProductos {
 	struct nodoListaProductos* primerElemento;
@@ -51,10 +56,20 @@ int mostrarDisponibles(infoProducto infoProductos[1000], int cantProductos);
 int masVendido(rankingProducto rankingProductos[1000], int cantProductos);
 
 //funciones secundarias
-int ordenarRanking(int rankingProductos[1000], int cantProductos);
-int intercambiar_struct(rankingProducto rankingProductosA, rankingProducto rankingProductosB);
+int inicializarRanking();
+int actualizarRanking();
+int ordenarRanking(rankingProducto rankingProductos[1000], int cantProductos);
+int intercambiar_struct(rankingProducto* rankingProductosA, rankingProducto* rankingProductosB);
 
 
+int crearListaTicket(nodoListaProductos* listaCarrito, nodoListaTicket* listaTicket);
+
+
+int buscarProducto(infoProducto infoProductos[1000], int cantProductos, int codProducto);
+int existeProducto(infoProducto infoProductos[1000], int cantProductos, int codProducto);
+int hayStock(infoProducto infoProductos[1000], int cantProductos, int codProducto);
+int agregarCarrito(infoProducto infoProductos[1000], int indice, nodoListaProductos* listaCarrito, int cant);
+int sacarDeStock(infoProducto infoProductos[1000], int indice, int cant);
 
 
 
@@ -70,6 +85,7 @@ int main(void) {
 
 	printf("\nCargue los productos:\n");
 	cargarProductos(infoProductos, &cantProductos);	//una idea seria hacerlo por archivo csv
+	inicializarRanking();
 	printf("\n\n");
 	printf("Menu\n");
 	printf("1. Atender cliente\n");
@@ -189,34 +205,47 @@ int cargarProductos(infoProducto infoProductos[1000], int * cantProductos) {
 
 int atenderCliente(int* nroCliente, int* contadorClientes, infoProducto infoProductos[1000], int* cantProductos, rankingProducto rankingProductos[1000]) {
 	
-	cant_productos_carrito = 0;
-	listaProductos* listaCarrito = NULL;
-	listaProductos* listaTicket = NULL
+	int codProducto;
+	int cant_productos_carrito = 0;
+	nodoListaProductos* listaCarrito = NULL;
+	nodoListaTicket* listaTicket = NULL;
 	int indice, cant, stock;
 
 	//arma el carrito del cliente con una lista y saca los correspondientes al array
 	printf("ingrese codigo del producto, 0 para terminar: ");
 	scanf("%d", &codProducto);
-	while ( codProducto !=0 && existeProducto(infoProductos, codProducto) && hayStock(infoProductos, codProducto) ) {
-		printf("producto no disponible.\n");
-		printf("ingrese codigo del producto, 0 para terminar: ");
-		scanf("%d", &codProducto);
+	while (codProducto == 0 && !existeProducto(infoProductos, *cantProductos, codProducto) && !hayStock(infoProductos, *cantProductos, codProducto) ) {
+		if (codProducto == 0) {
+			printf("debe llevar al menos un producto.\n");
+			printf("ingrese codigo del producto, 0 para terminar: ");
+			scanf("%d", &codProducto);
+		}
+		else {
+			printf("producto no disponible.\n");
+			printf("ingrese codigo del producto, 0 para terminar: ");
+			scanf("%d", &codProducto);
+		}
 	}
 	
-	while (codProducto != 0 && cant_productos_carrito < 1 && cant_productos_carrito > 100) {
+	while (codProducto != 0 && cant_productos_carrito < 100) {
 
-		indice = buscarProducto(infoProductos, codProducto);
-		stock = infoProducto[indice].cantidad;
+		indice = buscarProducto(infoProductos, *cantProductos, codProducto);
+		//printf("\n indice %d\n", indice);
+		stock = infoProductos[indice].cantidad;
+		//printf("\n stock %d\n", stock);
 
 		printf("ingrese la cantidad a llevar: ");
+		fflush(stdin);
 		scanf("%d", &cant);
-		while (cant < stock) {
+		while (cant < 1 || cant > stock) {
 			printf("cantidad no valida.");
 			printf("ingrese la cantidad a llevar: ");
 			scanf("%d", &cant);
 		}
 
-		agregarListaCarrito(infoProductos, indice, listaCarrito);
+		agregarCarrito(infoProductos, indice, listaCarrito, cant);
+		
+		
 		sacarDeStock(infoProductos, indice, cant);
 
 
@@ -224,7 +253,7 @@ int atenderCliente(int* nroCliente, int* contadorClientes, infoProducto infoProd
 
 		printf("ingrese codigo del producto, 0 para terminar: ");
 		scanf("%d", codProducto);
-		while ( codProducto != 0 && existeProducto(infoProductos, codProducto) && hayStock(infoProductos, codProducto)) {
+		while (!existeProducto(infoProductos, *cantProductos, codProducto) && !hayStock(infoProductos, *cantProductos, codProducto)) {
 			printf("producto no disponible.\n");
 			printf("ingrese codigo del producto, 0 para terminar: ");
 		}
@@ -233,12 +262,167 @@ int atenderCliente(int* nroCliente, int* contadorClientes, infoProducto infoProd
 
 	//prepara una lista de cant x precio (listaticket)
 	//si no existe en la lista, crea nodo y agrega, sino, agrega a existente (modifica elemento)
+	crearListaTicket(listaCarrito, listaTicket);
 
 
 	//imprime esa lista, actualiza el ranking y la borra
-	imprimirTicket(listaTicket);
-	actualizarRanking(infoProductos, listaTicket)
+	//imprimirTicket(listaTicket);
+	actualizarRanking(infoProductos, listaTicket);
 	
+}
+
+int existeProducto(infoProducto infoProductos[1000],int cantProductos,int codProducto) {
+	int i;
+	int existe = 0;
+	for (i = 0; i < cantProductos; i++) {
+		if (infoProductos[i].id == codProducto) {
+			existe = 1;
+			//printf("existe!");
+		}
+	}
+	return existe;
+}
+int hayStock(infoProducto infoProductos[1000], int cantProductos, int codProducto) {
+	int i;
+	int haystock = 0;
+	for (i = 0; i < cantProductos; i++) {
+		if (infoProductos[i].id == codProducto && infoProductos[i].cantidad > 0) {
+			haystock = 1;
+			//printf("hay stock!");
+		}
+	}
+	return haystock;
+}
+
+int buscarProducto(infoProducto infoProductos[1000], int cantProductos, int codProducto) {
+	int i = 0;
+	while (i < cantProductos && codProducto != infoProductos[i].id) {
+	
+		i = i + 1;
+	}
+	return i;
+}
+
+int agregarCarrito(infoProducto infoProductos[1000], int indice, nodoListaProductos* listaCarrito, int cant) {
+
+	nodoListaProductos* elementoNuevo;
+	printf("holii");
+	if (listaCarrito == NULL)
+	{
+		elementoNuevo = (nodoListaProductos*)malloc(sizeof(nodoListaProductos));
+		//insertarPrincipio(listaDisponibles, elementoNuevo)
+		elementoNuevo->datos.id = infoProductos[indice].id;
+		//puts
+		//elementoNuevo->nombre = infoProductos[i].nombre;
+		elementoNuevo->datos.precio = infoProductos[indice].precio;
+		elementoNuevo->datos.cantidad = infoProductos[indice].cantidad;
+		elementoNuevo->siguiente = NULL;
+
+		listaCarrito = elementoNuevo;
+		printf("soy un elemento nuevo");
+	}
+	else {
+		
+		elementoNuevo = (nodoListaProductos*)malloc(sizeof(nodoListaProductos));
+		//insertarPrincipio(listaDisponibles, elementoNuevo)
+		elementoNuevo->datos.id = infoProductos[indice].id;
+		//puts
+		//elementoNuevo->nombre = infoProductos[i].nombre;
+		elementoNuevo->datos.precio = infoProductos[indice].precio;
+		elementoNuevo->datos.cantidad = infoProductos[indice].cantidad;
+		elementoNuevo->siguiente = NULL;
+
+		//inserta ppio
+		elementoNuevo->siguiente = listaCarrito;
+		listaCarrito = elementoNuevo;
+		printf("soy otro elemento nuevo");
+	}
+}
+
+int crearListaTicket(nodoListaProductos* listaCarrito, nodoListaTicket* listaTicket) {
+	
+	int esta;
+
+	int id;
+	char nombre[30];
+	float precio;
+	int cantidad;
+	float total;
+
+	nodoListaProductos* pActualListaCarrito = NULL;
+	nodoListaTicket* pActualListaTicket = NULL;
+	nodoListaTicket* nodoNuevo = NULL;
+
+
+	if (listaCarrito != NULL) {
+		//crear nuevo nodo para listaTicket y engancharlo
+		pActualListaCarrito = listaCarrito;
+
+		while (listaCarrito->siguiente != NULL) {
+
+
+
+			//buscar si esta en listaTicket
+			esta = 0;
+			pActualListaTicket = listaTicket;
+			if (pActualListaTicket->datos.id == pActualListaCarrito->datos.id) {
+				esta = 1;
+			}
+			while (listaTicket->siguiente != NULL && !esta) {
+				if (pActualListaTicket->datos.id == pActualListaCarrito->datos.id) {
+					esta = 1;
+				}
+			}
+			// si NO, 
+			//crear nuevo nodo para listaTicket y engancharlo
+			if (!esta) {
+				nodoNuevo = (nodoListaTicket*)malloc(sizeof(nodoListaTicket));
+				nodoNuevo->datos.id = pActualListaCarrito->datos.id;
+				//nodoNuevo->datos.nombre = pActualListaCarrito->datos.nombre;
+				//usar puts
+				nodoNuevo->datos.precio = pActualListaCarrito->datos.precio;
+				nodoNuevo->datos.cantidad = pActualListaCarrito->datos.cantidad;
+				nodoNuevo->datos.total = precio*cantidad;
+				nodoNuevo->siguiente = NULL;
+				if (listaTicket == NULL) {
+					listaTicket = nodoNuevo;
+				}
+				else {
+					nodoNuevo->siguiente = listaTicket;
+					listaTicket = nodoNuevo;
+				}
+			}
+
+			// si SI,
+			//sumarle la cantidad y hacer sumar al total el precio*cant
+			if (esta) {
+				pActualListaTicket->datos.cantidad = pActualListaTicket->datos.cantidad + cantidad;
+				pActualListaTicket->datos.total = pActualListaTicket->datos.total + precio * cantidad;
+			}
+
+
+
+			pActualListaCarrito = pActualListaCarrito->siguiente;
+		}
+
+
+	}
+	else {
+		printf("Error, carrito vacio.");
+	}
+
+}
+
+
+int sacarDeStock(infoProducto infoProductos[1000], int indice, int cant) {
+
+	infoProductos[indice].cantidad = infoProductos[indice].cantidad - cant;
+
+}
+
+
+int actualizarRanking(infoProducto rankingProductos[1000], nodoListaTicket* listaTicket) {
+
 }
 
 
@@ -319,17 +503,22 @@ int mostrarDisponibles(infoProducto infoProductos[1000], int cantProductos) {
 
 
 
-int masVendido(rankingProducto rankingProductos[1000], cantProductos) {
+int inicializarRanking() {
+
+}
+
+int masVendido(rankingProducto rankingProductos[1000],int cantProductos) {
 	
 	//ordena el ranking (masvendido a menosvendido)
-	ordenarRanking(rankingProductos[1000], cantProductos);
+	ordenarRanking(rankingProductos, cantProductos);
 	//imprimir el primero del array de ranking
-	printf("El producto mas vendido es: %d", rankingProductos[0].nombre);
-	printf("Que vendio un total de: %d", rankingProductos[0].cantidad);
+	printf("El producto mas vendido es: %d", rankingProductos[0].id);
+	printf("Que vendio un total de: %d", rankingProductos[0].ventas);
 	
 }
 
 int ordenarRanking(rankingProducto rankingProductos[1000], int cantProductos) {
+	int i = 0;
 
 	while (i < cantProductos) {
 		int j = 0;
@@ -344,15 +533,15 @@ int ordenarRanking(rankingProducto rankingProductos[1000], int cantProductos) {
 	return 0;
 }
 
-int intercambiar_struct(rankingProducto* elemRankingProductosA, rankingProducto* elemRankingProductoB) {
+int intercambiar_struct(rankingProducto* elemRankingProductosA, rankingProducto* elemRankingProductosB) {
 
 	int aux;
-	aux = elemRankingProductosA.id;
-	elemRankingProductosA.id = elemRankingProductosB.id;
-	elemRankingProductosB.id = aux;
+	aux = elemRankingProductosA->id;
+	elemRankingProductosA->id = elemRankingProductosB->id;
+	elemRankingProductosB->id = aux;
 
-	aux = elemRankingProductosA.ventas;
-	elemRankingProductosA.ventas = elemRankingProductosB.ventas;
-	elemRankingProductosB.ventas = aux;
+	aux = elemRankingProductosA->ventas;
+	elemRankingProductosA->ventas = elemRankingProductosB->ventas;
+	elemRankingProductosB->ventas = aux;
 
 }
